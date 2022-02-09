de50ae1af9eaa4093603301404da29cf54749c21
Adam Kapuściński <ziemniaczek320@gmail.com>
2022-02-09 23:18:08 +0100

    przywrócenie stanu sprzed 2 commitów + poprawki w web.xml. Zachowany stan W User.java

--- src/main/java/pl/ias/pas/hotelroom/pasrest/security/JwtAuthMechanism.java
+++ src/main/java/pl/ias/pas/hotelroom/pasrest/security/JwtAuthMechanism.java
@@ -32,9 +32,6 @@ public class JwtAuthMechanism implements HttpAuthenticationMechanism {
         if (request.getRequestURL().toString().endsWith("login") || request.getRequestURL().toString().endsWith("test")) {
             return context.doNothing();
         }
-        if (request.getMethod().contains("OPTIONS")){
-            return context.doNothing();
-        }
 
         String authorizationHeader = request.getHeader(AUTHORIZATION_HEADER);
         if (authorizationHeader == null || !authorizationHeader.startsWith(BEARER_PREFIX)) {
